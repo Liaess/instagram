ca0f7465751944169c19bddad032e58738696108
Yann <yann.melo@gmail.com>
2021-04-30 01:15:13 -0300

    Sidebar

--- src/Corpo/SecaoPosts.js
+++ src/Corpo/SecaoPosts.js
@@ -1,48 +1,48 @@
 const posts = [
-    {nome:"meowed", imagem:"assets/img/meowed.svg", conteudo:"assets/img/gato-telefone.svg", curtidaImagem:"assets/img/respondeai.svg", curtidausuario: "respondeai", usercontador: "outras 101.523 pessoas"},
-    {nome:"meowed", imagem:"assets/img/barked.svg", conteudo:"assets/img/dog.svg", curtidaImagem:"assets/img/adorable_animals.svg", curtidausuario: "adorable_animals", usercontador: "outras 99.159 pessoas"}
+    {nome:"meowed", imagem:"assets/img/meowed.svg", conteudo:"assets/img/gato-telefone.svg", curtidaImagem:"assets/img/respondeai.svg", curtidasUser: "respondeai", userContador: "outras 101.523 pessoas"},
+    {nome:"meowed", imagem:"assets/img/barked.svg", conteudo:"assets/img/dog.svg", curtidaImagem:"assets/img/adorable_animals.svg", curtidasUser: "adorable_animals", userContador: "outras 99.159 pessoas"}
 ]
 
 export default function SecaoPosts(){
-    return(
-        <div class="posts">
-            {posts.map((posts)=>
-            <div class="post">
-                <div class="topo">
-                    <div class="usuario">
-                        <img src={posts.imagem} />
-                        {posts.nome}
-                    </div>
-                    <div class="acoes">
-                        <ion-icon name="ellipsis-horizontal"></ion-icon>
-                    </div>
-                </div>
-                <div class="conteudo">
-                    <img src={posts.conteudo} />
-                </div>
-                <div class="fundo">
-                    <div class="acoes">
-                    <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                    </div>
-
-                    <div class="curtidas">
-                    <img src={posts.curtidaImagem} />
-                    <div class="texto">
-                        Curtido por <strong>{posts.curtidausuario}</strong> e <strong>{posts.usercontador}</strong>
-                    </div>
-                    </div>
-                </div>
+  return(
+    <div class="posts">
+      {posts.map((posts)=>
+      <div class="post">
+        <div class="topo">
+            <div class="usuario">
+                <img src={posts.imagem} />
+                {posts.nome}
             </div>
-            )}
+            <div class="acoes">
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+            </div>
         </div>
-    )    
+        <div class="conteudo">
+          <img src={posts.conteudo} />
+        </div>
+        <div class="fundo">
+          <div class="acoes">
+          <div>
+              <ion-icon name="heart-outline"></ion-icon>
+              <ion-icon name="chatbubble-outline"></ion-icon>
+              <ion-icon name="paper-plane-outline"></ion-icon>
+          </div>
+          <div>
+              <ion-icon name="bookmark-outline"></ion-icon>
+          </div>
+          </div>
+
+          <div class="curtidas">
+          <img src={posts.curtidaImagem} />
+          <div class="texto">
+              Curtido por <strong>{posts.curtidasUser}</strong> e <strong>{posts.userContador}</strong>
+          </div>
+          </div>
+        </div>
+      </div>
+      )}
+    </div>
+  )    
 }
 
 
